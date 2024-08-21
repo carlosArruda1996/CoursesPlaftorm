00aea4d4e905c8c2dbed08a3180f4c17814fbba2
carlosArruda1996 <edu.arruda.759@gmail.com>
2024-08-21 18:49:51 -0300

    final version

--- DesafioDioPOO/src/dominio/classroom/Teacher.java
+++ DesafioDioPOO/src/dominio/classroom/Teacher.java
@@ -2,9 +2,31 @@ package dominio.classroom;
 
 import dominio.bootcamp.courses.Course;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Teacher {
 
     private String name;
 
-    private Course course;
+    private Set<Course> courses = new LinkedHashSet<>();
+
+    public  Teacher(String name, Course course){
+        this.name = name;
+        if(courses.contains(course)){
+            courses.add(course);
+        }
+    }
+    public Teacher(String name){
+        this.name = name;
+    }
+    public Teacher(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
